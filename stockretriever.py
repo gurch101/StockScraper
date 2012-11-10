bf2b0d1ce6fd13b60ec0ba891a62214896588915
gurch101 <gurch101@gmail.com>
2012-11-10 15:35:06 -0800

    remove dependencies

--- stockretriever.py
+++ stockretriever.py
@@ -1,34 +1,63 @@
-import sys
+import sys, httplib, urllib
 
-import yahoo.yql
+try: import simplejson as json
+except ImportError: import json
+
+PUBLIC_API_URL = 'http://query.yahooapis.com/v1/public/yql'
+DATATABLES_URL = 'store://datatables.org/alltableswithkeys'
+HISTORICAL_URL = 'http://ichart.finance.yahoo.com/table.csv?s='
+RSS_URL = 'http://finance.yahoo.com/rss/headline?s='
+FINANCE_TABLES = {'quotes': 'yahoo.finance.quotes',
+                 'options': 'yahoo.finance.options',
+                 'quoteslist': 'yahoo.finance.quoteslist',
+                 'sectors': 'yahoo.finance.sectors',
+                 'industry': 'yahoo.finance.industry'}
+
+class YQLQuery(object):
+
+	def __init__(self):
+		self.connection = httplib.HTTPConnection('query.yahooapis.com')
+
+
+
+	def execute(self, yql):
+		queryString = urllib.urlencode({'q': yql, 'format': 'json', 'env': DATATABLES_URL})
+		self.connection.request('GET', PUBLIC_API_URL + '?' + queryString)
+		return json.loads(self.connection.getresponse().read())
+
+
 
 class QueryError(Exception):
+
 	def __init__(self, value):
 		self.value = value
+
+
+
 	def __str__(self):
 		return repr(self.value)
         
-class StockRetriever(yahoo.yql.YQLQuery):
+
+
+class StockRetriever(YQLQuery):
     """A wrapper for the Yahoo! Finance YQL api."""
-    
-    HISTORICALURL = 'http://ichart.finance.yahoo.com/table.csv?s='
-    RSSURL = 'http://finance.yahoo.com/rss/headline?s='
-    FINANCETABLES = {'quotes': 'yahoo.finance.quotes',
-                     'options': 'yahoo.finance.options',
-                     'quoteslist': 'yahoo.finance.quoteslist',
-                     'sectors': 'yahoo.finance.sectors',
-                     'industry': 'yahoo.finance.industry'}
                      
     def __init__(self):
         super(StockRetriever, self).__init__()
 
+
+
     def __format_symbol_list(self, symbolList):
 		return ",".join(["\""+stock+"\"" for stock in symbolList])
 	
+
+
     def __is_valid_response(self, response, field):
         return 'query' in response and 'results' in response['query'] \
             and field in response['query']['results']
     
+
+
     def __validate_response(self, response, tagToCheck):
         if self.__is_valid_response(response, tagToCheck):
             quoteInfo = response['query']['results'][tagToCheck]
@@ -39,6 +68,8 @@ class StockRetriever(yahoo.yql.YQLQuery):
             else:
                 raise QueryError('YQL response malformed.')
         return quoteInfo
+
+
         
     def get_current_info(self, symbolList, columnsToRetrieve='*'):
         """Retrieves the latest data (15 minute delay) for the 
@@ -48,75 +79,88 @@ class StockRetriever(yahoo.yql.YQLQuery):
         symbols = self.__format_symbol_list(symbolList)
 
         yql = 'select %s from %s where symbol in (%s)' \
-              %(columns, self.FINANCETABLES['quotes'], symbols)
+              %(columns, FINANCE_TABLES['quotes'], symbols)
         response = super(StockRetriever, self).execute(yql)
         return self.__validate_response(response, 'quote')
+
+
     
     def get_historical_info(self, symbol):
         """Retrieves historical stock data for the provided symbol.
-        Historical data includes day open, close, high, low, volume,
+        Historical data includes date, open, close, high, low, volume,
         and adjusted close."""
+        
         yql = 'select * from csv where url=\'%s\'' \
               ' and columns=\"Date,Open,High,Low,Close,Volume,AdjClose\"' \
-               % (self.HISTORICALURL+symbol)
+               % (HISTORICAL_URL + symbol)
         results = super(StockRetriever, self).execute(yql)
         # delete first row which contains column names
         del results['query']['results']['row'][0]
         return results['query']['results']['row']
         
+
+
     
     def get_news_feed(self, symbol):
         """Retrieves the rss feed for the provided symbol."""
         
-        feedUrl = self.RSSURL+symbol
+        feedUrl = RSS_URL + symbol
         yql = 'select title, link, description, pubDate from rss where url=\'%s\'' % feedUrl
         response = super(StockRetriever, self).execute(yql)
         if response['query']['results']['item'][0]['title'].find('not found') > 0:
             raise QueryError('Feed for %s does not exist.' % symbol)
         else:
             return response['query']['results']['item']
+
+
         
     def get_options_info(self, symbol, expiration='', columnsToRetrieve ='*'):
         """Retrieves options data for the provided symbol."""
         
         columns = ','.join(columnsToRetrieve)
         yql = 'select %s from %s where symbol = \'%s\'' \
-              % (columns, self.FINANCETABLES['options'], symbol)
+              % (columns, FINANCE_TABLES['options'], symbol)
         
         if expiration != '':
             yql += " and expiration='%s'" %(expiration)
         
         response = super(StockRetriever, self).execute(yql)
-        print response
         return self.__validate_response(response, 'optionsChain')
+
+
         
     def get_index_summary(self, index, columnsToRetrieve='*'):
         columns = ','.join(columnsToRetrieve)
         yql = 'select %s from %s where symbol = \'@%s\'' \
-              % (columns, self.FINANCETABLES['quoteslist'], index)
+              % (columns, FINANCE_TABLES['quoteslist'], index)
         response = super(StockRetriever, self).execute(yql)
         return self.__validate_response(response, 'quote')
+
+
     
     def get_industry_ids(self):
         """retrieves all industry names and ids."""
         
-        yql = 'select * from %s' % self.FINANCETABLES['sectors']
+        yql = 'select * from %s' % FINANCE_TABLES['sectors']
         response = super(StockRetriever, self).execute(yql)
         return self.__validate_response(response, 'sector')
+
+
     
     def get_industry_index(self, id):
         """retrieves all symbols that belong to an industry."""
         
         yql = 'select * from %s where id =\'%s\'' \
-              % (id, self.FINANCETABLES['industry'])
+              % (id, FINANCE_TABLES['industry'])
         response = super(StockRetriever, self).execute(yql)
         return self.__validate_response(response, 'industry')
         
+
 if __name__ == "__main__":
     retriever = StockRetriever()
     try:
-        #print retriever.getCurrentData(sys.argv[1:])
-        print retriever.get_industry_ids()
+        print retriever.get_current_info(sys.argv[1:])
+        #print retriever.get_industry_ids()
         #print retriever.get_news_feed('yhoo')
     except QueryError, e:
         print e
